daa25ba290497dd6c5a0e58d281e8a8e8fcfea75
Davi Gouveia Vilela <75736172+Davilela@users.noreply.github.com>
2021-10-04 15:41:57 -0300

    Add files via upload

---
+++ App notas com firebase/src/app/app.firebase.config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB4kKvvCW15gLj4EiVkiUDrdZ-_MW0CoQg",
+  authDomain: "autenticacao-ionic5.firebaseapp.com",
+  databaseURL: "https://autenticacao-ionic5-default-rtdb.firebaseio.com",
+  projectId: "autenticacao-ionic5",
+  storageBucket: "autenticacao-ionic5.appspot.com",
+  messagingSenderId: "1086133639931",
+  appId: "1:1086133639931:web:6e354e3e642d560267ee5e",
+  measurementId: "G-LXJZ222674"
+};  
---
+++ App notas com firebase/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { UtilService } from './services/util.service';
+
+//Firebase
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+
+import { firebaseConfig } from './app.firebase.config';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    AngularFireStorageModule,
+    ReactiveFormsModule],
+  providers: [UtilService, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
