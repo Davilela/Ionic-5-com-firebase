8a2f10d4d9c399eece1b4a7729e34f110d8fb1f4
Davi Gouveia Vilela <75736172+Davilela@users.noreply.github.com>
2021-10-04 15:45:53 -0300

    Add files via upload

---
+++ App notas com firebase/src/app/register/register.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from "@angular/fire/compat/auth";
+import { NavController } from '@ionic/angular';
+import { User } from '../../models/user';
+import { UtilService } from '../services/util.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+  user = {} as User;
+  constructor(
+    private navCtrl: NavController,
+    private afAuth: AngularFireAuth,
+    private utilCtrl: UtilService) { }
+
+  ngOnInit() {
+  }
+
+  async register(user: User) {
+    this.afAuth.createUserWithEmailAndPassword(user.email, user.password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        this.navCtrl.navigateForward('home');
+      })
+      .catch((error) => {
+      this.utilCtrl.showToast(error.message);
+      });
+  }
+}
